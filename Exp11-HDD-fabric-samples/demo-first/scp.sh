f396cafe8c9314e030e57b39241eb6cfd1dfb5f5
Canhui Wang, Ph.D. <320653941@qq.com>
2026-07-28 10:44:45 +0800

    Commit

---
+++ Exp11-HDD-fabric-samples/demo-first/scp.sh
@@ -0,0 +1,83 @@
+#--------------------------------------------------------------------------------------------------
+# The first time takes a few minutes
+#--------------------------------------------------------------------------------------------------
+# The demo-frist folder
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.6:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.9:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.10:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.14:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.16:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.17:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.19:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.21:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.23:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.24:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.25:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.33:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.40:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.66:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.72:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.74:$HOME/fabric-samples/
+scp -r $HOME/fabric-samples/demo-first/ $USER@10.0.3.76:$HOME/fabric-samples/
+
+# The chaincode folder
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.6:$HOME/fabric-samples/chaincode/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.9:$HOME/fabric-samples/chaincode/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.10:$HOME/fabric-samples/chaincode/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.14:$HOME/fabric-samples/chaincode/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.16:$HOME/fabric-samples/chaincode/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.17:$HOME/fabric-samples/chaincode/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.19:$HOME/fabric-samples/chaincode/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.21:$HOME/fabric-samples/chaincode/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.23:$HOME/fabric-samples/chaincode/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.24:$HOME/fabric-samples/chaincode/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.25:$HOME/fabric-samples/chaincode/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.33:$HOME/fabric-samples/chaincode/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.40:$HOME/fabric-samples/chaincode/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.66:$HOME/fabric-samples/chaincode/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.72:$HOME/fabric-samples/chaincode/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.74:$HOME/fabric-samples/chaincode/abstore/go
+scp -r $HOME/fabric-samples/chaincode/abstore/go $USER@10.0.3.76:$HOME/fabric-samples/chaincode/abstore/go
+
+#--------------------------------------------------------------------------------------------------
+# Copy config file
+#--------------------------------------------------------------------------------------------------
+# The config file
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.6:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.9:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.10:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.14:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.16:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.17:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.19:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.21:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.23:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.24:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.25:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.33:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.40:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.66:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.72:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.74:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/channel-artifacts/ $USER@10.0.3.76:$HOME/fabric-samples/demo-first/
+
+
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.6:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.9:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.10:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.14:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.16:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.17:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.19:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.21:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.23:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.24:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.25:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.33:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.40:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.66:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.72:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.74:$HOME/fabric-samples/demo-first/
+scp -r $HOME/fabric-samples/demo-first/crypto-config/ $USER@10.0.3.76:$HOME/fabric-samples/demo-first/
+
+
